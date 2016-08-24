dacc519fc713d422dc37ab5735669dc2fb0d1894
benstone1 <benjamin.h.stone@gmail.com>
2016-08-24 12:25:28 -0400

    revised 8-24 exercises

---
+++ lessons/arrays/exercises/8-24-array-exercises.playground/Contents.swift
@@ -0,0 +1,124 @@
+//1)
+//Create an array of strings called colors that contain "orange", "red", "yellow", "turquoise", and "lavender"
+//Using array subscripting and string interpolation, print out the String "orange, yellow, and lavender are some of my favorite colors"
+
+//Write all your code below:
+
+
+
+//2)
+//Remove "Illinois" and "Kansas" from the array below.
+
+var westernStates = ["California", "Oregon", "Washington", "Idaho", "Illinois", "Kansas"]
+
+
+//Write all your code below:
+
+//3)
+//Iterate through the array below.  For each each state, print out whether or not it is in the continental United States.
+
+let moreStates = ["Hawaii", "New Mexico", "Alaska", "Montana", "Texas", "New York", "Florida"]
+
+//Write all your code below:
+
+
+//4)
+//a) print out how many non-whitespace characters are in myString
+let myString = "This is practice for the next problem!"
+
+//Write all your code below:
+
+
+//b) Iterate through the array below.  For each sentence, print out how many non-whitespace characters are in it.
+
+let myFavoriteQuotes = ["To be or not to be, that is the question.", "The only source of knowledge is experience.", "Mr. Gorbachev, tear down this wall!", "Four score and twenty years ago..."]
+
+//Write all your code below:
+
+
+
+//5)
+//The below array represents an unfinished batting lineup for a baseball team. You, the coach, need to make some last minute changes.
+
+//Add "Suzuki" to the end of your lineup.
+//Change "Jeter" to "Tejada".
+//Change "Thomas" for "Guerrero"
+//Put "Reyes" to bat 8th instead.
+var battingLineup = ["Reyes", "Jeter", "Ramirez", "Pujols","Griffey","Thomas","Jones", "Rodriguez"]
+
+//Write all your code below:
+
+
+//6)
+//Iterate through the garden and place any 🌷 that you find into the basket.  Replace any 🌷 that you pick up with "dirt".  Then print how many 🌷 are in your basket.
+var garden = ["dirt","🌷","dirt","🌷","dirt","dirt","🌷","dirt","🌷","dirt"]
+var basket = [String]()
+
+//Write all your code below:
+
+//7)
+//Iterating through listOfNumbers and print out the largest element.
+var listOfNumbers = [1, 2, 3, 10, 100, 13, 14, 31]
+
+//Write all your code below:
+
+
+//8)
+//Iterate through secondListOfNumbers, and print out all the odd numbers.
+var secondListOfNumbers = [19,13,14,19,101,10000,141,404]
+
+//Write all your code below:
+
+//9)
+//Iterate through thirdListOfNumbers, and print out the sum.
+var thirdListOfNumbers = [11, 26, 49, 61, 25, 40, 74, 3, 22, 23]
+
+//Write all your code below:
+
+
+//10)
+//Iterate through the array and check to see if there is at least one integer that equals target.  Then, print "YES" if you found a match, and "NO" if you didn't
+
+let target = 84
+var fourthListOfNumbers = [83, 1, 66, 64, 90, 22, 97, 10, 84, 27]
+
+
+//Write all your code below:
+
+
+//11)
+//Append every Int that appears in both listOne and listTwo to the sharedElements array.  Then print how many Ints are shared.
+var listOne = [28, 64, 7, 96, 13, 32, 94, 11, 80, 68]
+var listTwo = [18, 94, 48, 6, 42, 68, 79, 76, 13, 7]
+var sharedElements = [Int]()
+
+
+//Write all your code below:
+
+
+
+//12)
+//Write code such that noDupeList has all the same Ints as dupeFriendlyList, but has no more than one of each Int.
+var dupeFriendlyList = [4,2,6,2,2,6,4,9,2,1]
+var noDupeList: [Int] = []
+
+//13)
+//Find the second smallest integer in ages
+
+var ages = [53, 31, 88, 65, 25, 44, 77, 18, 24, 84, 46, 42, 50, 28, 78, 67, 83, 70, 38, 69, 66, 71, 68, 61, 86, 85, 41, 15, 81, 40]
+
+
+//14)
+//Print out the sum of the diagonals of myMatrix
+
+var myMatrix = [[10, 14, 12], [91, 1, 9], [31, 3, 21]]
+
+
+//15)
+//Challenge problem!
+//Using for loops, rotate myMatrix 90 degrees (https://sharecode.io/assets/problem_images/2518_5.jpg)
+
+var toRotate = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+
