0ca419e4e4f25825fe3dbc691c8507c04c7f1ded
Ben Stone <benjamin.h.stone@gmail.com>
2016-08-24 14:17:26 -0400

    Update Contents.swift

--- lessons/arrays/exercises/8-24-array-exercises.playground/Contents.swift
+++ lessons/arrays/exercises/8-24-array-exercises.playground/Contents.swift
@@ -103,8 +103,8 @@ var dupeFriendlyList = [4,2,6,2,2,6,4,9,2,1]
 var noDupeList: [Int] = []
 
 //13)
-//Find the second smallest integer in ages
-
+//Find the second smallest Int in ages
+//Then find the third largest Int
 var ages = [53, 31, 88, 65, 25, 44, 77, 18, 24, 84, 46, 42, 50, 28, 78, 67, 83, 70, 38, 69, 66, 71, 68, 61, 86, 85, 41, 15, 81, 40]
 
 
@@ -135,5 +135,10 @@ var someRepeats = [25, 11, 30, 31,50,28,4,37,13,20,24,38,28,14,44,33,7,43,39,35,
 
 var someRepeatsAgain = [25,11,30,31,50,28,4,37,13,20,24,38,28,14,44,33,7,43,39,35,36,42,1,40,7,14,23,46,21,39,11,42,12,38,41,48,20,23,29,24,50,41,38,23,11,30,50,13,13,16,10,8,3,43,10,20,28,39,24,36,21,13,40,25,37,39,31,4,46,20,38,2,7,11,11,41,45,9,49,31,38,23,41,16,49,29,14,6,6,11 ,5 ,39, 13 ,17,43,1,1,15,25]
 
+//19)
+//Identify if there are 3 integers in the following array that sum to 10.  If so, print them.
+//If there are multiple triplets, print all possible triplets.
+
+var tripleSumArr = [-20,-14, -8,-5,-3,-2,1,2,3,4,9,15,20,30]
 
 
