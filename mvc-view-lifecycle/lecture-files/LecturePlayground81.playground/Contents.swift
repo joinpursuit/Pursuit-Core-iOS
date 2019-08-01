d67604427cff94ca7bfeb319e87a8c48bed110d7
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-01 15:48:55 -0400

    Adds afternoon protocol lesson code

--- mvc-view-lifecycle/lecture-files/LecturePlayground81.playground/Contents.swift
+++ mvc-view-lifecycle/lecture-files/LecturePlayground81.playground/Contents.swift
@@ -59,57 +59,60 @@ var str = "Hello, playground"
 */
 
 //name tells us what it's job is (loosely)
-protocol FullyNamed {
+//protocol FullyNamed {
     //properties are computed because the protocol itself doesnt' store values
     //in protocol, you only have to say what type of computed values the property will have (read-only or read-write)
-    var fullName: String { get }
-}
+//    var fullName: String { get }
+//}
+//
+//struct Person: FullyNamed {
+//    //to conform to the protocol, what do i need?
+//    //i will need to implement all properties and methods of that protocol!
+//    var fullName: String
+//}
+//
+//struct Dog: FullyNamed {
+//    var barkSound: String
+//    var fullName: String
+//
+//}
+//
+//var alyson = Person(fullName: "Alyson Abril")
 
-struct Person: FullyNamed {
-    //to conform to the protocol, what do i need?
-    //i will need to implement all properties and methods of that protocol!
-    var fullName: String
-}
 
-struct Dog: FullyNamed {
-    var barkSound: String
-    var fullName: String
-    
-}
-
-var alyson = Person(fullName: "Alyson Abril")
-
-
-protocol Foodlike {
-    var poisonous: Bool { get }
-    var consistency: String { get set }
-    var calories: Int { get set }
-    func munch() -> Int
-}
-
-protocol Seedy {
-    var canGrowPlant: Bool { get set }
-    var shape: String { get }
-}
-
-struct SunflowerSeed: Foodlike, Seedy {
-    let poisonous: Bool = false
-    var consistency: String
-    var calories: Int
-    
-    var canGrowPlant: Bool
-    var shape: String
-    
-    func munch() -> Int {
-        return calories
-    }
-}
-
-let sfls = SunflowerSeed(consistency: "crunchy awesome", calories: 6, canGrowPlant: true, shape: "wavy teardrop")
-
-//print(sfls)
-let oneMouthful = sfls.munch()
-print("I just chewed \(oneMouthful) calories of that \(sfls.consistency) sunflower seed ")
+//protocol Foodlike {
+//    var poisonous: Bool { get }
+//    var consistency: String { get set }
+//    var calories: Int { get set }
+//    func munch() -> Int
+//}
+//
+//protocol Seedy {
+//    var canGrowPlant: Bool { get set }
+//    var shape: String { get }
+//}
+//
+//struct SunflowerSeed: Foodlike, Seedy {
+//    // poisonous is a constant, so you can only get it by its implementation here.
+//    let poisonous: Bool = false
+//    var consistency: String
+//    var calories: Int
+//
+//    var canGrowPlant: Bool
+//    var shape: String //you CAN get it, so that's good. you can also set it
+//
+//    func munch() -> Int {
+//        return calories
+//    }
+//}
+//
+//var sfls = SunflowerSeed(poisonous: true, consistency: "crunchy awesome", calories: 6, canGrowPlant: true, shape: "wavy teardrop")
+//
+//sfls.consistency = "fluffy"
+//sfls.shape = "nonwavy teardrop"
+////print(sfls)
+//let oneMouthful = sfls.munch()
+//print("I just chewed \(oneMouthful) calories of that \(sfls.consistency) sunflower seed ")
 
 //ambiguous bc it might also mean scary/unsavory
 // struct SunflowerSeed
@@ -118,3 +121,77 @@ print("I just chewed \(oneMouthful) calories of that \(sfls.consistency) sunflow
 
 //Delegation is usually a good choice when a type needs to be usable in many different contexts
 
+//get only property ex: array.count
+
+
+protocol FullyNamed {
+//change this up by getting rid of fullName - give it a couple properties that, when implemented, make up each part of the name (a couple different strings) and an instance method that will return the full name as a string
+    var firstName: String { get set }
+    var middleName: String? { get set }
+    var lastName: String { get set }
+    
+    func fullName() -> String
+    
+}
+
+//create a struct called Person that conforms to the protocol FullyNamed.
+
+struct Person : FullyNamed, Comparable, CustomStringConvertible {
+    var description: String {
+        get  {
+            if let bindedMiddleName = middleName {
+                return "\(firstName) \(bindedMiddleName) \(lastName)"
+            } else {
+                return "\(firstName) \(lastName)"
+            }
+        }
+    }
+    
+    static func < (lhs: Person, rhs: Person) -> Bool {
+        return lhs.netWorth > rhs.netWorth
+    }
+    
+    var netWorth: Int
+    var firstName: String
+    var middleName: String?
+    var lastName: String
+    
+    init(firstName: String, lastName: String){
+        //leaves out the middle name
+        self.firstName = firstName
+        self.lastName = lastName
+        self.netWorth = -10000
+    }
+    
+    init(firstName: String, middleName: String, lastName: String){
+        //give person a middle name
+        self.firstName = firstName
+        self.middleName = middleName
+        self.lastName = lastName
+        self.netWorth = -10000
+    }
+    
+    func fullName() -> String {
+        if let bindedMiddleName = middleName {
+            return "\(firstName) \(bindedMiddleName) \(lastName)"
+        } else {
+            return "\(firstName) \(lastName)"
+        }
+    }
+}
+
+let person1 = Person(firstName: "John", lastName: "Doe")
+let person2 = Person(firstName: "John", middleName: "A", lastName: "Seed")
+
+//print(person1.fullName())
+
+print(person1)
+
+/*
+ - `Equatable`
+ - `Comparable`
+ - `Hashable`
+ - `Collection`
+ - `Sequence`
+ - `CustomStringConvertible`
+ */
